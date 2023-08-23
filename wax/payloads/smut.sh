911a1ef0c7a8a7bb9ad6b635ffaaa4b3ece49f6e
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-08-23 07:07:12 -0500

    Remove todo

--- wax/payloads/smut.sh
+++ wax/payloads/smut.sh
@@ -64,7 +64,7 @@ vpd -i RW_VPD block_devmode=0
 clear
 
 echo "Found the following recovery images:"
-ls -lh /usr/local/smut-reco # TODO: find correct path for smut payloads
+ls -lh /usr/local/smut-reco
 echo
 echo "Starting SMUT in 5 seconds..."
 sleep 5
