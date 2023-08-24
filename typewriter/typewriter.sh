6452b301bc6b4f1bcd62cce6db7e9d4ed0828428
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-08-24 03:18:05 +0000

    Typewriter POC

---
+++ typewriter/typewriter.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Welcome to typewriter.sh, a SMUT shim-building automation tool"
+echo "Made by rainestorme"
+
+echo "Before you start, confirm that you have the following files in the /typewriter folder:"
+echo " - raw_shim.bin - your raw shim for the board you want to build for"
+echo " - reco_image.bin - your v105 recovery image for the board you want to build for"
+echo "Also make sure this script was run as root! (TODO add root check)"
+echo " Press enter to continue, Ctrl+C to exit"
+read -r
+
+echo "Copying reco image (this will take a while)..."
+cp ./reco_image.bin ./murkmod_image.bin
+
+echo "Patching murmod reco image..."
+./image_patcher.sh ./murkmod_image.bin
+
+echo "Moving reco images to smut-reco payloads folder..."
+mkdir -p ../wax/smut-reco
+mv ./murkmod_image.bin ../wax/smut-reco/murkmod_v105.bin
+mv ./reco_image.bin ../wax/smut-reco/recovery_v105.bin
+
+echo "Moving shim to wax folder..."
+mv ./raw_shim.bin ../wax/raw_shim.bin
+
+echo "Building shim (this will take a while)..."
+pushd ../wax/
+    wget https://dl.sh1mmer.me/build-tools/chromebrew/chromebrew-dev.tar.gz
+    bash wax.sh $(pwd)/raw_shim.bin --dev
+popd
+
+mv ../wax/raw_shim.bin ./sh1mmer_smut.bin
+
+echo "Everything should be done, your new shim should be located in the /typewriter directory!"
