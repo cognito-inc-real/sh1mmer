d8609125748840149ea3f4775df04f53aff22913
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-11-26 15:08:07 -0600

    0x8091

--- wax/payloads/defog.sh
+++ wax/payloads/defog.sh
@@ -3,7 +3,7 @@ if crossystem wp_sw?1; then
     echo "WP not disabled - try on v105 with no battery"
     return
 fi
-/usr/share/vboot/bin/set_gbb_flags.sh 0x8090
+/usr/share/vboot/bin/set_gbb_flags.sh 0x8091
 crossystem block_devmode=0
 vpd -i RW_VPD block_devmode=0
 echo "GBB flags set. Devmode should now be unblocked, you're good to proceed"
